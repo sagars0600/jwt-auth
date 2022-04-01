b510d5b0ecd5bf06eb46e144eed50f49badde845
senil <senila@maveric-systems.com>
2022-04-01 09:41:32 +0530

    added auth

---
+++ src/main/java/com/auth/Autherication/AuthericationApplication.java
@@ -0,0 +1,13 @@
+package com.auth.Autherication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthericationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthericationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/Autherication/AuthericationApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.auth.Autherication;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AuthericationApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
